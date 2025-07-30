5c555c977300724c0f3e3fe27c4e38fcc73075bd
Umeshdk07 <umeshrdk1997@gmail.com>
2025-07-31 01:32:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("/home/umesh/.ssh/authorized_keys") 
+    public_key = file("/home/mdpdev/.ssh/authorized_keys") 
   }
     
 source_image_reference {
