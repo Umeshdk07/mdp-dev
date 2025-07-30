14574e387a384b9b5856f8922cc0e3df3f907f9a
Umeshdk07 <umeshrdk1997@gmail.com>
2025-07-30 23:25:30 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,51 @@
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "public_ip_name" {
+  description = "Name of the public IP address"
+  type        = string
+}
+
+variable "nic_name" {
+  description = "Name of the network interface"
+  type        = string
+}
+
+variable "nsg_name" {
+  description = "Name of the network security group"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for the virtual machine"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the virtual machine"
+  type        = string
+  sensitive   = true
+}
